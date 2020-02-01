565d1d33c027c9fe7087d1c740f7c7327ba61c0a
loucadufault <40028187+loucadufault@users.noreply.github.com>
2020-02-01 16:48:22 -0500

    Update Question_A.py
    added a public function to return if two lines supplied as a tuple of endpoints are overlapping, added docstrings and comments

--- Question_A/Question_A.py
+++ Question_A/Question_A.py
@@ -1,20 +1,29 @@
 from test_data import *
 
 class Line:
+    """A 1D line along the x-axis that is delimited by a start and an end point. """
     def __init__(self, start, end):
-        self.start = min(start, end)
-        self.end = max(start, end)
+        self.start = min(start, end) # choose the minimum of the end points as start point to support out of order parameters
+        self.end = max(start, end) # likewise choose the maximum
 
     def __repr__(self):
+        """Representation of the line object as a string mimicking the contructor call."""
         return "Line({}, {})".format(self.start, self.end)
     
     def __str__(self):
         return self.__repr__()
-        #print("Line from {} to {} on the x-axis.".format(self.start, self.end))
 
     def overlaps(self, line):
+        """Method to check if the line is overlapping with the line passed as argument.
+        Returns True if the lines are overlapping (even in a single point), returns False otherwise."""
+        
         return not (self.start > line.end or self.end < line.start)
 
+def overlap(line1_endpoints, line2_endpoints):
+    """Public function to return if two lines supplied as two tuple of enpoints overlap."""
+    
+    return Line(*line1_endpoints).overlaps(Line(*line2_endpoints)) # create anonymous objects
+    
 def overlap_validator(lines: tuple, oracle: bool):
     assert lines[0].overlaps(lines[1]) == lines[1].overlaps(lines[0])
     assert lines[0].overlaps(lines[1]) == oracle
